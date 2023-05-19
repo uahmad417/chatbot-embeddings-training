c16a8c624a7355c8eaa84d308a90c16073e69a8a
uahmad417 <18u.ahmad@gmail.com>
2023-05-19 17:18:21 +0500

    updated embedding script to embed tracks tracks data

--- embed.py
+++ embed.py
@@ -2,26 +2,51 @@ import json
 import openai
 import os
 import tqdm
+from time import sleep
 
 # Set up OpenAI API key
-openai.api_key = "INSERT_API_KEY_HERE"
+openai.api_key = "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"
 
 # Define model and parameters
 model_engine = "text-embedding-ada-002"
 
-with open('pages.json') as f:
-    pages = json.load(f)
-for i, page in enumerate(pages):
+with open('tracks.json') as f:
+    tracks = json.load(f)
+
+# to track track count for each genre
+
+genre_count = {
+    "pop": 0,
+    "country": 0,
+    "blues": 0,
+    "jazz": 0,
+    "reggae": 0,
+    "rock": 0,
+    "hip hop": 0
+}
+
+for i, track in enumerate(tracks):
     # Generate embeddings
-    response = openai.Embedding.create(
-        model=model_engine,
-        input=page['page']
-    )
-    # Extract embeddings from response
-    embedding = response["data"][0]["embedding"]
 
-    # add the embeddings to the pages array
-    pages[i]['embedding'] = embedding
+    # upper limit of each genre is 8
+    # so from all tracks retrieve 8 tracks from each genre
+    # this is done to stay within the rate limit of openai api
+    if genre_count[track["genre"]] < 8:
 
-with open('pages_with_embeddings.json', 'w') as f:
-    json.dump(pages, f)
+        # increase current count of the genre
+        genre_count[track["genre"]] += 1
+
+        response = openai.Embedding.create(
+            model=model_engine,
+            input=track['lyrics']
+        )
+        # Extract embeddings from response
+        embedding = response["data"][0]["embedding"]
+
+        # add the embeddings to the tracks array
+        tracks[i]['embedding'] = embedding
+    else:
+        continue
+
+with open('tracks_with_embeddings.json', 'w') as f:
+    json.dump(tracks, f)
